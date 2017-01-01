b65f7a22b6ee91addca82ef43d1f7e2d7faa5499
Emanuele-Tedesco <manutede@gmail.com>
2017-01-01 15:39:48 +0100

    Update functions.php

--- contents/functions.php
+++ contents/functions.php
@@ -47,7 +47,7 @@
 		$mail->isSMTP();                                      // Set mailer to use SMTP
 		$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 		$mail->SMTPAuth = true;                               // Enable SMTP authentication
-		$mail->Username = 'st.taurus.service@gmail.com';                 // SMTP username
+		$mail->Username = '********';                 // SMTP username
 		$mail->Password = '********';                           // SMTP password
 		$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 		$mail->Port = 465;                                    // TCP port to connect to
